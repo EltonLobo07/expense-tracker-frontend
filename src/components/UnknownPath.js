ef59486b3ebb81d47e7ddf384fd47cc41f54d015
Elton Lobo <eltonsw00@gmail.com>
2022-09-16 11:33:08 +0530

    Complete UnknownPath component

--- src/components/UnknownPath.js
+++ src/components/UnknownPath.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function UnknownPath() {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Unknown path
+        <div className = "h-screen flex flex-col justify-center items-center bg-gray-50 gap-y-4">
+            <div className = "text-lg">
+                Unknown path
+            </div>
+
+            <button className = "btn btn-v1" onClick = {() => navigate("/categories")}>
+                Go to categories
+            </button>
         </div>
     );
 };
